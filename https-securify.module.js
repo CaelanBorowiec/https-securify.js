a6f8c4dea5709a6dc4772bd613a1786d3f02af31
Caelan Borowiec <caelan@caelanb.me>
2021-07-28 10:32:48 -0400

    Support for img tags, and basic support for script tags (#1)
    * initial work on script tags
    
    * Enable script tag src replacement
    
    * Fix scripts not actually getting their src.value updated.
    
    * handling for img tags

--- https-securify.module.js
+++ https-securify.module.js
@@ -1,4 +1,6 @@
 const securify = function(elems) {
+  console.log(elems)
+
   for (var i = 0; i < elems.length; i++) {
     let elem = elems[i];
     switch (elem.nodeName) {
@@ -7,11 +9,24 @@ const securify = function(elems) {
         if (href.split(':')[0] == "http")
           elem.href = href.replace("http", "https");
         break;
+      case 'IMG':
+      case 'SCRIPT':
+        // TODO: find a way to handle $(document).ready scripts failing
+        if (!elem.attributes.hasOwnProperty('src'))
+          break;
+        let src = elem.attributes.src.value;
+        if (src.split(':')[0] == "http")
+          elem.attributes.src.value = src.replace("http", "https");
+        break;
       default:
         console.log(elem)
     }
+
   }
+
 }
 if (window.location.protocol == "https:") {
   securify(document.getElementsByTagName('link'));
+  securify(document.getElementsByTagName('script'));
+  securify(document.getElementsByTagName('img'));
 }
