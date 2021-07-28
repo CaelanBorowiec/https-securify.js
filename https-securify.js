6c9269a99dc8c662207666fe6502f4435e6400d4
Caelan Borowiec <caelan@caelanb.me>
2021-07-28 15:44:36 -0400

    Add comments and minified version (#4)
    * initial work on script tags
    
    * Enable script tag src replacement
    
    * Fix scripts not actually getting their src.value updated.
    
    * handling for img tags
    
    * support for anchor tags
    
    * Major rewrite using a MutationObserver
    
    * remove todo
    
    * remove superfluous window props
    
    * add comments
    
    * Create https-securify.min.js

--- https-securify.js
+++ https-securify.js
@@ -1,17 +1,20 @@
 const securify = function(mutationsList, observer) {
   for (const mutation of mutationsList) {
+    // For each element added to the DOM:
     for (var i = 0; i < mutation.addedNodes.length; i++) {
       let elem = mutation.addedNodes[i];
-
+      // Check to see what it is, and modify appropriately
       switch (elem.nodeName) {
         case 'A':
         case 'LINK':
+          // A and LINK tags both use the href property, so they can be handled the same
           let href = elem.href;
           if (href.split(':')[0] == "http")
             elem.href = href.replace("http", "https");
           break;
         case 'IMG':
         case 'SCRIPT':
+          // Likewise IMG and SCRIPT tags both use the SRC prop
           if (!elem.attributes.hasOwnProperty('src'))
             break;
           let src = elem.attributes.src.value;
@@ -24,5 +27,6 @@ const securify = function(mutationsList, observer) {
 };
 
 if (window.location.protocol == "https:") {
+  // If we're running on a https page, create a MutationObserver and pass any DOM changes to the document to securify()
   new MutationObserver(securify).observe(document, { childList: true, subtree: true });
 }
