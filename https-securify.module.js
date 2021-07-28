a3761403b547eaa522fcdf4710a42fa330c82b63
Caelan Borowiec <caelan@caelanb.me>
2021-07-28 10:15:28 -0400

    initial work on script tags

--- https-securify.module.js
+++ https-securify.module.js
@@ -1,4 +1,6 @@
 const securify = function(elems) {
+  console.log(elems)
+
   for (var i = 0; i < elems.length; i++) {
     let elem = elems[i];
     switch (elem.nodeName) {
@@ -7,11 +9,22 @@ const securify = function(elems) {
         if (href.split(':')[0] == "http")
           elem.href = href.replace("http", "https");
         break;
+      case 'SCRIPT':
+        console.log(elem)
+        if (!elem.attributes.hasOwnProperty('src'))
+          break;
+        let src = elem.attributes.src.value;
+        if (src.split(':')[0] == "http")
+          console.log(src)
+        break;
       default:
         console.log(elem)
     }
+
   }
+
 }
 if (window.location.protocol == "https:") {
-  securify(document.getElementsByTagName('link'));
+  // securify(document.getElementsByTagName('link'));
+  securify(document.getElementsByTagName('script'));
 }
